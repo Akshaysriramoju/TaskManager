87fb92bc1caf3d15dba306ca542fe41798b9fb2f
Akshay <sriramojuakshay361@gmail.com>
2025-10-14 19:30:24 +0530

    update

--- src/test/java/com/example/taskmanager/TaskManagerApplicationTests.java
+++ src/test/java/com/example/taskmanager/TaskManagerApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.taskmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Use application-test.properties
 class TaskManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // This will load the Spring context with H2 in-memory DB
+    }
 
 }
